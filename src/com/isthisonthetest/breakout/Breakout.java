e4414d75baec6d64500ffc00a60c960dfaeedcb5
Cody Herring <cody.herring@gmail.com>
2012-02-08 01:35:12 -0500

    refactored methods for clearer program flow

--- src/com/isthisonthetest/breakout/Breakout.java
+++ src/com/isthisonthetest/breakout/Breakout.java
@@ -70,6 +70,13 @@ public class Breakout extends BaseGameActivity
 	private Sound mPaddleHit;
 	private Sound mDeath;
 
+	// the point where the engine starts drawing the tiles
+	int startX;
+	// the width of the blocks to be drawn
+	final int blockWidth = 32;
+	// number of rows to draw on the scene
+	final int numRows = 5;
+
 	// this is run when the game is first loaded.
 	// i.e. when the application is launched
 	@Override
@@ -95,43 +102,11 @@ public class Breakout extends BaseGameActivity
 	@Override
 	public void onLoadResources()
 	{
-		// initializes all of the texture regions
-		// do not change this area unless you know what you're doing
-		this.mTexture = new Texture(256, 32,
-				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-		this.mPongTextureRegion = TextureRegionFactory.createFromAsset(
-				this.mTexture, this, "gfx/pong.png", 0, 0);
-		this.mBallTextureRegion = TextureRegionFactory.createTiledFromAsset(
-				this.mTexture, this, "gfx/ball.png", 97, 0, 1, 1);
-		this.mBoxTextureRegion = TextureRegionFactory.createFromAsset(
-				this.mTexture, this, "gfx/box.png", 114, 0);
-		// end initialize textures
+		initTextures();
 
-		// sets the root for the sound effects to the mfx folder
-		SoundFactory.setAssetBasePath("mfx/");
-		// initialize all the sounds
-		try
-		{
-			this.mTileHit = SoundFactory.createSoundFromAsset(
-					this.mEngine.getSoundManager(), this, "crush.ogg");
-			this.mPaddleHit = SoundFactory.createSoundFromAsset(
-					this.mEngine.getSoundManager(), this, "paddlehit.ogg");
-			this.mDeath = SoundFactory.createSoundFromAsset(
-					this.mEngine.getSoundManager(), this, "death.ogg");
-		} catch (Exception e)
-		{
-			Debug.e("Error: " + e);
-		}
+		initSounds();
 
-		// sets up font for scoreboard
-		this.mFontTexture = new Texture(256, 256, TextureOptions.BILINEAR);
-		this.mFont = new Font(this.mFontTexture, Typeface.create(
-				Typeface.DEFAULT, Typeface.BOLD), 48, true, Color.BLACK);
-		this.mEngine.getTextureManager().loadTexture(this.mFontTexture);
-		this.mEngine.getFontManager().loadFont(this.mFont);
-
-		// loads the texture for the entire screen
-		this.mEngine.getTextureManager().loadTexture(this.mTexture);
+		setupScoreBoard();
 	}
 
 	@Override
@@ -158,22 +133,7 @@ public class Breakout extends BaseGameActivity
 		// these are used to set the position for the paddle sprite
 		final int centerX = (CAMERA_WIDTH - this.mPongTextureRegion.getWidth()) / 2;
 		final int centerY = CAMERA_HEIGHT - this.mPongTextureRegion.getHeight();
-		final Sprite paddle = new Sprite(centerX, centerY,
-				this.mPongTextureRegion)
-		{
-			@Override
-			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
-					final float pTouchAreaLocalX, final float pTouchAreaLocalY)
-			{
-				// this gets the X coordinate of the finger touch,
-				// then subtracts the width of the paddle,
-				// setting the paddle location to the bottom even if
-				// the touch is not directly on top of the paddle.
-				this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2,
-						centerY);
-				return true;
-			}
-		};
+		final Sprite paddle = setupPaddle(centerX, centerY);
 
 		// creates the ball object that bounces around
 		final Ball ball = new Ball(CAMERA_WIDTH / 2, CAMERA_HEIGHT / 3,
@@ -185,31 +145,7 @@ public class Breakout extends BaseGameActivity
 		scene.getTopLayer().addEntity(paddle);
 		scene.getTopLayer().addEntity(ball);
 
-		// the point where the engine starts drawing the tiles
-		int startX;
-		// the width of the blocks to be drawn
-		final int blockWidth = 32;
-		// number of rows to draw on the scene
-		final int numRows = 5;
-
-		// this places blocks until there is no more room on the screen
-		for (int h = 0; h < numRows * blockWidth; h += blockWidth)
-		{
-			startX = 10;
-			for (int i = 0; i < Math.floor(CAMERA_WIDTH / blockWidth); i++)
-			{
-				Sprite boxSprite = new Sprite(startX, 10 + h,
-						this.mBoxTextureRegion);
-				boxSpritesList.add(boxSprite);
-				startX += blockWidth;
-			}
-		}
-
-		// adds all boxes to the screen
-		for (Sprite boxSprite : boxSpritesList)
-		{
-			scene.getTopLayer().addEntity(boxSprite);
-		}
+		fillBoxArea(scene);
 
 		// adds a listener for the paddle in order to receive input
 		scene.registerTouchArea(paddle);
@@ -231,12 +167,9 @@ public class Breakout extends BaseGameActivity
 				// this subtracts 1 point from the score
 				// this may be changed to have more drastic consequences such as
 				// restarting the entire game
-				if (ball.getY() + ball.getBaseHeight() > paddle.getY() * 1.05)
+				if (ballBelowPaddle(paddle, ball))
 				{
-					Breakout.this.mDeath.play();
-					score -= 1;
-					scoreText.setText("Score: " + score);
-					ball.setPosition(CAMERA_WIDTH / 2, CAMERA_HEIGHT / 3);
+					playerDeath(ball);
 				}
 
 				// reflect the ball when it hits the paddle
@@ -246,78 +179,101 @@ public class Breakout extends BaseGameActivity
 					ball.setVelocityY(-DEMO_VELOCITY);
 				}
 
-				final List<Sprite> toRemoved = new ArrayList<Sprite>();
-				boxCheckLoop: for (Sprite box : boxSpritesList)
-				{
-					// if the ball collides with the box
-					// AND it hits the bottom specifically
-					if (ball.collidesWith(box))
-					{
-						// increment the score and change the text
-						score++;
-						scoreText.setText("Score: " + score);
+				final List<Sprite> toRemoved = checkForCollisions(ball);
 
-						// add the box to the list of sprites to remove
-						toRemoved.add(box);
-						// play the "tile hit" sound
-						Breakout.this.mTileHit.play();
-						// the tolerance for collision detection
-						float eps = 0.2f;
-						// coordinates for top left of the ball
-						float aX = ball.getX();
-						float aY = ball.getY();
-						// height and width of the ball
-						float aH = ball.getBaseHeight();
-						float aW = ball.getBaseWidth();
-						// coordinates for top left of the collided box
-						float bX = box.getX();
-						float bY = box.getY();
-						// height and width of the collided box
-						float bH = box.getBaseHeight();
-						float bW = box.getBaseWidth();
-
-						// bottom of ball hits the top of a block
-						if (Math.abs((aY + aH) - bY) <= bH * eps)
-						{
-							ball.setVelocityY(-ball.getVelocityY());
-						} // top of ball hits the bottom of a block
-						else if (Math.abs(aY - (bH + bY)) <= bH * eps)
-						{
-							ball.setVelocityY(-ball.getVelocityY());
-						} // right side of ball hits the left side of a block
-						else if (Math.abs((aX + aW) - bX) <= bW * eps)
-						{
-							ball.setVelocityX(-ball.getVelocityX());
-						} // left side of ball hits the right side of a block
-						else if (Math.abs(aX - (bW + bX)) <= bW * eps)
-						{
-							ball.setVelocityX(-ball.getVelocityX());
-						}
-
-						break boxCheckLoop;
-					}
-
-				}
-
-				// this runs once per update/frame
-				Breakout.this.runOnUpdateThread(new Runnable()
-				{
-
-					@Override
-					public void run()
-					{
-						for (Sprite sprite : toRemoved)
-						{
-							// remove all of the sprites in the "toRemoved" list
-							scene.getTopLayer().removeEntity(sprite);
-							boxSpritesList.remove(sprite);
-						}
-					}
-				});
+				removeSprite(scene, toRemoved);
 			}
+
 		});
 
 		return scene;
+	}
+
+	private List<Sprite> checkForCollisions(final Ball ball)
+	{
+		final List<Sprite> toRemoved = new ArrayList<Sprite>();
+		boxCheckLoop: for (Sprite box : boxSpritesList)
+		{
+			// if the ball collides with the box
+			// AND it hits the bottom specifically
+			if (ball.collidesWith(box))
+			{
+				// increment the score and change the text
+				score++;
+				scoreText.setText("Score: " + score);
+
+				// add the box to the list of sprites to remove
+				toRemoved.add(box);
+				// play the "tile hit" sound
+				Breakout.this.mTileHit.play();
+				checkCollisionDirection(ball, box);
+
+				break boxCheckLoop;
+			}
+		}
+		return toRemoved;
+	}
+
+	private void removeSprite(final Scene scene, final List<Sprite> toRemoved)
+	{
+		// this runs once per update/frame
+		Breakout.this.runOnUpdateThread(new Runnable()
+		{
+
+			@Override
+			public void run()
+			{
+				for (Sprite sprite : toRemoved)
+				{
+					// remove all of the sprites in the "toRemoved" list
+					scene.getTopLayer().removeEntity(sprite);
+					boxSpritesList.remove(sprite);
+				}
+			}
+		});
+	}
+
+	private void fillBoxArea(final Scene scene)
+	{
+		// this places blocks until there is no more room on the screen
+		for (int h = 0; h < numRows * blockWidth; h += blockWidth)
+		{
+			startX = 10;
+			for (int i = 0; i < Math.floor(CAMERA_WIDTH / blockWidth); i++)
+			{
+				Sprite boxSprite = new Sprite(startX, 10 + h,
+						this.mBoxTextureRegion);
+				boxSpritesList.add(boxSprite);
+				startX += blockWidth;
+			}
+		}
+
+		// adds all boxes to the screen
+		for (Sprite boxSprite : boxSpritesList)
+		{
+			scene.getTopLayer().addEntity(boxSprite);
+		}
+	}
+
+	private Sprite setupPaddle(final int centerX, final int centerY)
+	{
+		final Sprite paddle = new Sprite(centerX, centerY,
+				this.mPongTextureRegion)
+		{
+			@Override
+			public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+					final float pTouchAreaLocalX, final float pTouchAreaLocalY)
+			{
+				// this gets the X coordinate of the finger touch,
+				// then subtracts the width of the paddle,
+				// setting the paddle location to the bottom even if
+				// the touch is not directly on top of the paddle.
+				this.setPosition(pSceneTouchEvent.getX() - this.getWidth() / 2,
+						centerY);
+				return true;
+			}
+		};
+		return paddle;
 	}
 
 	@Override
@@ -364,5 +320,101 @@ public class Breakout extends BaseGameActivity
 
 			super.onManagedUpdate(pSecondsElapsed);
 		}
+	}
+
+	private void initSounds()
+	{
+		// sets the root for the sound effects to the mfx folder
+		SoundFactory.setAssetBasePath("mfx/");
+		// initialize all the sounds
+		try
+		{
+			this.mTileHit = SoundFactory.createSoundFromAsset(
+					this.mEngine.getSoundManager(), this, "crush.ogg");
+			this.mPaddleHit = SoundFactory.createSoundFromAsset(
+					this.mEngine.getSoundManager(), this, "paddlehit.ogg");
+			this.mDeath = SoundFactory.createSoundFromAsset(
+					this.mEngine.getSoundManager(), this, "death.ogg");
+		} catch (Exception e)
+		{
+			Debug.e("Error: " + e);
+		}
+	}
+
+	private void initTextures()
+	{
+		// initializes all of the texture regions
+		// do not change this area unless you know what you're doing
+		this.mTexture = new Texture(256, 32,
+				TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		this.mPongTextureRegion = TextureRegionFactory.createFromAsset(
+				this.mTexture, this, "gfx/pong.png", 0, 0);
+		this.mBallTextureRegion = TextureRegionFactory.createTiledFromAsset(
+				this.mTexture, this, "gfx/ball.png", 97, 0, 1, 1);
+		this.mBoxTextureRegion = TextureRegionFactory.createFromAsset(
+				this.mTexture, this, "gfx/box.png", 114, 0);
+		// end initialize textures
+
+		// loads the texture for the entire screen
+		this.mEngine.getTextureManager().loadTexture(this.mTexture);
+	}
+
+	private void setupScoreBoard()
+	{
+		// sets up font for scoreboard
+		this.mFontTexture = new Texture(256, 256, TextureOptions.BILINEAR);
+		this.mFont = new Font(this.mFontTexture, Typeface.create(
+				Typeface.DEFAULT, Typeface.BOLD), 48, true, Color.BLACK);
+		this.mEngine.getTextureManager().loadTexture(this.mFontTexture);
+		this.mEngine.getFontManager().loadFont(this.mFont);
+	}
+
+	private void checkCollisionDirection(final Ball ball, Sprite box)
+	{
+		// the tolerance for collision detection
+		float eps = 0.2f;
+		// coordinates for top left of the ball
+		float aX = ball.getX();
+		float aY = ball.getY();
+		// height and width of the ball
+		float aH = ball.getBaseHeight();
+		float aW = ball.getBaseWidth();
+		// coordinates for top left of the collided box
+		float bX = box.getX();
+		float bY = box.getY();
+		// height and width of the collided box
+		float bH = box.getBaseHeight();
+		float bW = box.getBaseWidth();
+
+		// bottom of ball hits the top of a block
+		if (Math.abs((aY + aH) - bY) <= bH * eps)
+		{
+			ball.setVelocityY(-ball.getVelocityY());
+		} // top of ball hits the bottom of a block
+		else if (Math.abs(aY - (bH + bY)) <= bH * eps)
+		{
+			ball.setVelocityY(-ball.getVelocityY());
+		} // right side of ball hits the left side of a block
+		else if (Math.abs((aX + aW) - bX) <= bW * eps)
+		{
+			ball.setVelocityX(-ball.getVelocityX());
+		} // left side of ball hits the right side of a block
+		else if (Math.abs(aX - (bW + bX)) <= bW * eps)
+		{
+			ball.setVelocityX(-ball.getVelocityX());
+		}
+	}
+
+	private void playerDeath(final Ball ball)
+	{
+		Breakout.this.mDeath.play();
+		score -= 1;
+		scoreText.setText("Score: " + score);
+		ball.setPosition(CAMERA_WIDTH / 2, CAMERA_HEIGHT / 3);
+	}
+
+	private boolean ballBelowPaddle(final Sprite paddle, final Ball ball)
+	{
+		return ball.getY() + ball.getBaseHeight() > paddle.getY() * 1.05;
 	}
 }
